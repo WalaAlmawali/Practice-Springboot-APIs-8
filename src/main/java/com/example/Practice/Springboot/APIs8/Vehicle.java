5051f96f7ecafca3184c9fddbe357cbf3671ffff
Wala AL mawali <wala.salam.9888@gmail.com>
2026-06-09 10:08:07 +0400

    Create Vehicle class

---
+++ src/main/java/com/example/Practice/Springboot/APIs8/Vehicle.java
@@ -0,0 +1,27 @@
+package com.example.Practice.Springboot.APIs8;
+
+public class Vehicle {
+    private String vehicleId;
+    private String vehicleModel;
+    private double rentalPricePerDay;
+
+    //Constructor
+    public Vehicle(String vehicleId, String vehicleModel, double rentalPricePerDay) {
+        this.vehicleId = vehicleId;
+        this.vehicleModel = vehicleModel;
+        this.rentalPricePerDay = rentalPricePerDay;
+    }
+
+    //Getter methods
+    public String getVehicleId() {
+        return vehicleId;
+    }
+
+    public String getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public double getRentalPricePerDay() {
+        return rentalPricePerDay;
+    }
+}
